6f12ee8a07c1d0664253fd9100d423e72f1544c6
Jean Michel Rouly <michel.rouly@gmail.com>
2014-04-17 18:37:04 -0400

    corrected directory location

--- code/preprocessing/strip_posts/strip_posts.sh
+++ code/preprocessing/strip_posts/strip_posts.sh
@@ -3,6 +3,14 @@
 set -e
 
 
+if [ "$2" = "" ]
+then
+  sourcedir="../../../data/data.toy/raw/"
+else
+  sourcedir=$2
+fi
+
+
 # S3 out protocol
 if [ "$1" = "s3" ]
 then
@@ -10,25 +18,27 @@ then
   # don't forget final /
   s3url="http://cs484project.s3.amazonaws.com/"
 
-  for input in ../../data/data/raw/*/Posts.xml
+  for input in $sourcedir/*/Posts.xml
   do
     outdir=$( echo $input | sed 's/^.*raw\//posts\//' | sed 's/Posts.xml//' )
     outurl="$s3url$outdir"
-    python strip_posts.py $1 $input $outurl
+    python strip_posts.py s3 $input $outurl
   done
-
-fi
-
 
 
 # disk out protocol
-if [ "$1" = "disk" ]
+elif [ "$1" = "disk" ]
 then
 
-  for input in ../../data/data/raw/*/Posts.xml
+  for input in $sourcedir/*/Posts.xml
   do
     output=$( echo $input | sed 's/raw/posts/' | sed 's/Posts.xml//' )
-    python strip_posts.py $1 $input $output
+    python strip_posts.py disk $input $output
   done
+
+
+else
+
+  echo "No protocol provided."
 
 fi
