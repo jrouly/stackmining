bb08e01d8b4c1478d0ff269c51df713b64027f41
Jean Michel Rouly <michel.rouly@gmail.com>
2014-04-17 17:50:21 -0400

    Added toy script as well

---
+++ code/preprocessing/strip_posts/strip_posts.toy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+
+# S3 out protocol
+if [ "$1" = "s3" ]
+then
+
+  # don't forget final /
+  s3url="http://cs484project.s3.amazonaws.com/"
+
+  for input in ../../data/data.toy/raw/*/Posts.xml
+  do
+    outdir=$( echo $input | sed 's/^.*raw\//posts\//' | sed 's/Posts.xml//' )
+    outurl="$s3url$outdir"
+    python strip_posts_to_s3.py $input $outurl
+  done
+
+fi
+
+
+
+# disk out protocol
+if [ "$1" = "disk" ]
+then
+
+  for input in ../../data/data.toy/raw/*/Posts.xml
+  do
+    output=$( echo $input | sed 's/raw/posts/' | sed 's/Posts.xml//' )
+    python strip_posts_to_disk.py $input $output
+  done
+
+fi
