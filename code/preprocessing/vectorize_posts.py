cfb866d904160b7d6cb6a6a52df0820fc084e659
Jean Michel Rouly <michel.rouly@gmail.com>
2014-04-17 23:27:26 -0400

    wrapped script in a function, return vectorized posts

--- code/preprocessing/vectorize_posts.py
+++ code/preprocessing/vectorize_posts.py
@@ -4,55 +4,92 @@ import time
 import logging
 
 import s3file
-import lxml.etree as xml
-import sklearn as sk
-import numpy as np
+import numpy
+
+from lxml.etree import parse
+from lxml.html import document_fromstring
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import Pipeline
 
 
-# turn on logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s ] %(levelname)s: %(message)s')
+def vectorize_posts(in_file, out_dest="null", protocol="null"):
 
-args = sys.argv
-num_args = len( sys.argv )
+    # turn on logging
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(levelname)s: %(message)s')
 
-if( num_args != 4 ):
-    logging.error( "Usage: python strip_posts.py [s3|disk] input_file output_dest" )
-    exit( 1 )
+    # catch edge case ouput protocols
+    if out_dest == "null" or protocol == "null" or protocol not in ["s3", "disk", "null"]:
 
-protocol = sys.argv[1]
-in_file = sys.argv[2]
-out_dest = sys.argv[3]
+        protocol = "null"
+        out_dest = "null"
 
-print( "Reading from..  %s\nWriting to....  %s" % (in_file, out_dest) )
+    logging.info( "Reading from..  %s" % in_file )
+    logging.info( "Writing to....  %s" % out_dest )
 
-if protocol == "disk":
-    # create output paths directory if it doesn't already exist
-    if not os.path.exists( out_dest ):
-        time.sleep( 1 ) # naively avoid race conditions
-        os.makedirs( out_dest )
-
-# construct tree over xml data
-tree = xml.parse( in_file )
-rows = tree.iter("row")
-
-# for each row, split out its contents and output
-rownum = 0
-for row in rows:
-
-    # generate new filename and contents
-    rownum = rownum + 1
-    filename = os.path.join( out_dest, "post" + str(rownum) )
-    body = row.get("Body").encode("ascii", "ignore")
-
-    # write body of post to file
     if protocol == "disk":
-        row_file = open( filename, 'w' )
-        row_file.write( body )
-        row_file.close()
+        # create output paths directory if it doesn't already exist
+        if not os.path.exists( out_dest ):
+            time.sleep( 1 ) # naively avoid race conditions
+            os.makedirs( out_dest )
 
-    # write body of post out to s3 bucket
-    elif protocol == "s3":
-        s3 = s3file.s3open( filename )
-        s3.write( body )
-        s3.close()
+    # construct tree over xml data
+    tree = parse( in_file )
+    rows = tree.iter("row")
+
+    # create a tf_idf vectorizer machine
+    tfidf_vectorizer = TfidfVectorizer(
+        input="content",
+        encoding="ascii",
+        decode_error="ignore",
+        strip_accents="ascii",
+        stop_words="english",
+        lowercase=True,
+        max_df=0.9,
+        use_idf=True,
+        smooth_idf=True,
+    )
+
+    posts = []
+
+    # for each row, split out its contents and output
+    rownum = 0
+    for row in rows:
+
+        # generate new filename and contents
+        rownum = rownum + 1
+        filename = os.path.join( out_dest, "post" + str(rownum) )
+        body = row.get("Body")
+
+        # skip empty documents
+        if len( body ) == 0:
+            continue
+
+        # strip out html
+        body = document_fromstring( body )
+        body = body.text_content()
+        body = body.encode("ascii", "ignore")
+
+        # add to list in memory
+        posts.append( body )
+
+        # write body of post to file
+        if protocol == "disk":
+            row_file = open( filename, 'w' )
+            row_file.write( body )
+            row_file.close()
+
+        # write body of post out to s3 bucket
+        elif protocol == "s3":
+            s3 = s3file.s3open( filename )
+            s3.write( body )
+            s3.close()
+
+        else:
+            pass
+
+    # remove HTML entities and perform stop word removal
+    vectorized_posts = tfidf_vectorizer.fit_transform( posts )
+
+    return vectorized_posts
