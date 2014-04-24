a98fbecaa88f63161e1bc5ef6ba74ade52673d01
Jean Michel Rouly <michel.rouly@gmail.com>
2014-04-24 00:53:58 -0400

    Added infrastructure for several possible clustering algorithms

---
+++ code/cluster.py
@@ -0,0 +1,62 @@
+"""
+Author: Michel Rouly
+Date:   2014-04-24
+"""
+
+
+import sys
+import logging
+
+from sklearn import metrics
+from sklearn.cluster import KMeans
+
+from read_corpus import read_corpus
+
+
+def do_kmeans( corpus ):
+    """
+    Do KMeans: perform KMeans clustering on an input corpus. Input is
+    expected to be a dictionary of categories to tf-idf vectors.
+    """
+
+    logging.info("Beginning KMeans clustering.")
+
+    return
+
+
+if __name__ == "__main__":
+
+    # turn on logging
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(levelname)s: %(message)s')
+
+    if len( sys.argv ) != 4:
+        logging.error( "Usage: python cluster.py [s3|disk] data_dir [kmeans]" )
+        sys.exit( 1 )
+
+    # pull in parameters from the command line
+    in_protocol = sys.argv[1]
+    data_dir = sys.argv[2]
+    algorithm = sys.argv[3]
+
+    # available algorithms
+    algorithms = ["kmeans",
+                  "ap",
+                  "meanshift",
+                  "spectral",
+                  "hierarchical",
+                  "dbscan",
+                  "gaussian"]
+
+    # read and vectorize the corpus
+    corpus = read_corpus( in_protocol=in_protocol, data_dir=data_dir )
+
+    if algorithm == "kmeans":
+        do_kmeans( corpus )
+
+    if algorithm == "ap":
+        do_ap( )
+
+    if algorithm == "meanshift":
+        do_meanshift( )
+
