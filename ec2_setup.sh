6c922eab7aca48810b1547b000ee04ad7e20b426
Jean Michel Rouly <michel.rouly@gmail.com>
2014-04-30 00:48:22 -0400

    Cleaned up script

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -2,12 +2,15 @@
 
 cd $HOME
 
-sudo apt-get install python-pip
+# System setup
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install python-dev libxml2-dev libxslt1-dev gfortran
+sudo apt-get install git python-pip vim
 sudo pip install virtualenv
-sudo apt-get install git
 
-sudo apt-get install python-dev libxml2-dev libxslt1-dev
-
+# Get code
 git clone http://git.gmu.edu/jrouly/cs484project.git
 
 cd cs484project
@@ -15,4 +18,9 @@ cd cs484project
 virtualenv-2.7 venv
 source venv/bin/activate
 
+sudo apt-get build-dep python-scipy
+
 pip install -r requirements.txt
+
+# ec2 creds
+source $HOME/.ec2/config
