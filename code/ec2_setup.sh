770bdf134082343b8a25c1e936cdbe4d2d27501f
Jean Michel Rouly <michel.rouly@gmail.com>
2014-05-05 02:55:37 -0400

    Tweaked ec2 script

--- code/ec2_setup.sh
+++ code/ec2_setup.sh
@@ -3,11 +3,11 @@
 cd $HOME
 
 # System setup
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
-sudo apt-get install python-dev libxml2-dev libxslt1-dev gfortran
-sudo apt-get install git python-pip vim
+sudo apt-get -y install python-dev libxml2-dev libxslt1-dev gfortran lib32z1-dev
+sudo apt-get -y install git python-pip vim
 sudo pip install virtualenv
 
 # Get code
@@ -18,9 +18,13 @@ cd cs484project
 virtualenv-2.7 venv
 source venv/bin/activate
 
-sudo apt-get build-dep python-scipy
+sudo apt-get -y build-dep python-scipy
 
 pip install -r requirements.txt
 
 # ec2 creds
-source $HOME/.ec2/config
+#cd $HOME
+#wget https://michel.rouly.net/public/cs484/ec2.tar.gz
+#tar xzvf ec2.tar.gz
+#mv ec2 .ec2
+#source $HOME/.ec2/config
